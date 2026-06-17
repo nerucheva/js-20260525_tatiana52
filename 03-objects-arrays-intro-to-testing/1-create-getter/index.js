0da26505283c1211c6f7a04e4428318289109b26
Tatiana Nerucheva <tatiana@nerucheva.com>
2026-06-17 20:49:32 +0200

    03-js-data-types: create getter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -3,6 +3,26 @@
  * @param {string} path - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
-export function createGetter(path) {
 
+export function createGetter(path) {
+  const objNodes = path.split(".");
+
+  return (obj) => {
+    if (!obj) {
+      return undefined;
+    }
+
+    let result = obj;
+
+    for (let i = 0; i < objNodes.length; i++) {
+      if (Object.hasOwn(result, objNodes[i])) {
+        result = result[objNodes[i]];
+      } else {
+        result = undefined;
+        break;
+      }
+    }
+
+    return result;
+  };
 }
