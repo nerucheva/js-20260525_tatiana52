7d3c60344e2e0210bd0d4bc05fc3f9ff29bfe56f
Tatiana Nerucheva <tatiana@nerucheva.com>
2026-06-18 22:19:24 +0200

    03-js-data-types: add invertObj

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,15 @@
  * @returns {object | undefined} - returns the new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+  if (!obj) {
+    return undefined;
+  }
+  const pairs = Object.entries(obj);
 
+  return pairs.reduce((acc, item) => {
+    acc[item[1]] = item[0];
+    return acc;
+  }, {});
 }
+
+console.log(invertObj({ 1: 2, 3: 4 }));
